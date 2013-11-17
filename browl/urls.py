ca584213a79897041a95f44f046048a7ba998af5
erkarl <karl@karlranna.com>
2013-11-17 12:40:46 +0200

    Added posts app and created a CRUD API endpoint for it.

--- browl/urls.py
+++ browl/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import patterns, include, url
+from rest_framework import routers
+from apps.posts.views import PostViewSet
 
 from django.contrib import admin
 admin.autodiscover()
 
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)
+
 urlpatterns = patterns('',
+                       url(r'^', include(router.urls)),
                        url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
                        url(r'^admin/', include(admin.site.urls)),
                        )
