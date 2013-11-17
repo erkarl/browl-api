8254df5aa5c33efacee39a9b2b80a089fab9a0a0
erkarl <karl@karlranna.com>
2013-11-17 12:19:24 +0200

    Configured browl to use PostgreSQL and added south.

---
+++ Makefile
@@ -0,0 +1,23 @@
+test: lint test-python
+
+lint:
+	@echo "Linting Python files"
+	flake8 --ignore=E121,W404,F403,E501 --exclude=./docs/*,./env/*,./venv/*,migrations,.git . || exit 1
+	@echo ""
+
+test-python:
+	@echo "Running Python tests"
+	python manage.py test || exit 1
+	@echo ""
+
+initdb:
+	python manage.py syncdb 
+	python manage.py migrate
+
+install:
+	pip install --upgrade setuptools
+	pip install --upgrade "flake8>=2.0"
+	pip install --upgrade -r requirements.txt
+
+server:
+	python manage.py runserver 0.0.0.0:7000
--- browl/settings.py
+++ browl/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +59,12 @@ WSGI_APPLICATION = 'browl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'browl',
+        'USER': 'browl',
+        'PASSWORD': 'browl',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
