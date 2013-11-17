1bbc8669ace30f578879f789dab40044e406ca22
erkarl <karl@karlranna.com>
2013-11-17 12:25:09 +0200

    Added django-rest-framework.

--- browl/urls.py
+++ browl/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'browl.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
