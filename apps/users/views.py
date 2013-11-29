964c5130e43bd219b8f15a1314e377819d431251
erkarl <karl@karlranna.com>
2013-11-29 17:12:09 +0200

    User API endpoint can only be accessed by Admins.

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 from .serializers import UserSerializer
 
 
@@ -9,3 +10,4 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
