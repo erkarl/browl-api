6e8d1fd5f799847973df4a128d74c1fd376ed611
erkarl <karl@karlranna.com>
2013-11-29 17:35:55 +0200

    Tests users API.

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,20 @@
+from django.core.urlresolvers import reverse
+from rest_framework.test import APITestCase, APIClient
+from django.contrib.auth.models import User
+from rest_framework import status
 # from django.test import TestCase
 
-# Create your tests here.
+
+class UserTest(APITestCase):
+
+    def setUp(self):
+            self.apiclient = APIClient()
+            self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'snow')
+
+    def test_get_user_list_unauthorized(self):
+        """
+        Ensure that unauthorized users won't be able to GET user list
+        """
+        url = reverse('user-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
