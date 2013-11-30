b6e06ff3a7dc23527879ef343d3eb4e590cd691b
erkarl <karl@karlranna.com>
2013-11-30 23:47:19 +0200

    Added more API test cases for users app.

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,15 +1,14 @@
 from django.core.urlresolvers import reverse
-from rest_framework.test import APITestCase, APIClient, APIRequestFactory
+from rest_framework.test import APITestCase, APIRequestFactory
 from django.contrib.auth.models import User
 from rest_framework import status
-# from django.test import TestCase
 
 
 class UserTest(APITestCase):
 
     def setUp(self):
-            self.apiclient = APIClient()
             self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'snow')
+            self.user = User.objects.create_user('tyrion', 'tyrion@lannister.com', 'lannister')
             self.factory = APIRequestFactory()
 
     def test_get_user_list_unauthorized(self):
@@ -17,17 +16,23 @@ class UserTest(APITestCase):
         Ensure that unauthorized users won't be able to GET user list
         """
         url = reverse('user-list')
-        # response = self.client.get(url)
-        request = self.factory.get(url, format='json')
-        # self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        import pdb
-        pdb.set_trace()
-        self.assertEqual(request.status_code, status.HTTP_403_FORBIDDEN)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_user_list_as_regular_user(self):
+        """
+        Ensure that regular users won't have access to GET user list
+        """
+        url = reverse('user-list')
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_get_user_list_as_admin(self):
         """
-        Ensure that unauthorized users won't be able to GET user list
+        Ensure that admin users have access to GET user list
         """
         url = reverse('user-list')
+        self.client.force_authenticate(user=self.superuser)
         response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
