e672f02e6acde932490d0f8985b12f4c241831ae
erkarl <karl@karlranna.com>
2013-12-02 15:01:21 +0200

    Posts API endpoint tests.

--- apps/posts/tests.py
+++ apps/posts/tests.py
@@ -1,3 +1,39 @@
-# from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework.test import APITestCase, APIRequestFactory
+from django.contrib.auth.models import User
+from rest_framework import status
 
-# Create your tests here.
+
+class UserTest(APITestCase):
+
+    def setUp(self):
+            self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'snow')
+            self.user = User.objects.create_user('tyrion', 'tyrion@lannister.com', 'lannister')
+            self.factory = APIRequestFactory()
+
+    def test_get_post_list_logged_out(self):
+        """
+        Ensure that everyone can GET posts-list
+        """
+        url = reverse('post-list')
+        # self.client.force_authenticate(user=self.user)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_post_list_regular_user(self):
+        """
+        Ensure that regular users can GET posts-list
+        """
+        url = reverse('post-list')
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_post_list_admin(self):
+        """
+        Ensure that superusers can GET posts-list
+        """
+        url = reverse('post-list')
+        self.client.force_authenticate(user=self.superuser)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
