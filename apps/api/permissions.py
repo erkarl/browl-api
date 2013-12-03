9b0dd8ee947e5d62f20244bd92692815d20b90f4
erkarl <karl@karlranna.com>
2013-12-03 14:30:01 +0200

    Refactored IsAccountAdminOrReadOnly.

--- apps/api/permissions.py
+++ apps/api/permissions.py
@@ -1,18 +1,16 @@
 from rest_framework import permissions
 
+SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
+
 
 class IsAccountAdminOrReadOnly(permissions.BasePermission):
     """
-    Allow creating, destroying and modifying the posts
-    for only admin users
+    The request is authenticated as an admin, or is a read-only request.
     """
 
     def has_permission(self, request, view):
-        if request.method == 'GET' or \
-           request.method == 'HEAD' or \
-           request.method == 'OPTIONS':
+        if (request.method in SAFE_METHODS or
+           request.user and
+           request.user.is_staff):
             return True
-        else:
-            if request.user and request.user.is_staff:
-                return True
-            return False
+        return False
